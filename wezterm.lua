daf63af1f00190fd67ce9298322b926d32a01cd2
Callum <callum.powley@gmail.com>
2024-12-10 22:26:18 +0000

    updated config

--- wezterm.lua
+++ wezterm.lua
@@ -3,11 +3,10 @@ local wezterm = require("wezterm")
 config = wezterm.config_builder()
 
 config = {
-	autonatically_reload_config = true,
-	enable_tab_bar = false,
+	automatically_reload_config = true,
 	default_cursor_style = "BlinkingBar",
 	color_scheme = 'Tokyo Night (Gogh)',
-	font_size = 14,
+	font_size = 16,
 	window_background_opacity = 0.9
 }
 
